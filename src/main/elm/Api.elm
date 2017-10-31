625befd2ec1e1358d1d76e873ef7e37f47417313
miyamoen <akihito.biblioteca@gmail.com>
2017-10-31 10:43:14 +0900

    Refactor Api.elm

--- src/main/elm/Api.elm
+++ src/main/elm/Api.elm
@@ -7,43 +7,71 @@ import Rocket exposing ((=>))
 import Debug
 
 
+type Method
+    = Get
+    | Post
+    | Put
+    | Delete
+
+
+request : Method -> List String -> List ( String, String ) -> RequestBuilder
+request method paths params =
+    let
+        url_ =
+            url paths params
+    in
+        case method of
+            Get ->
+                HttpBuilder.get url_
+
+            Post ->
+                HttpBuilder.post url_
+
+            Put ->
+                HttpBuilder.put url_
+
+            Delete ->
+                HttpBuilder.delete url_
+
+
 getGroupUsers : GroupId -> Cmd Msg
 getGroupUsers id =
-    let
-        toMsg result =
-            case result of
-                Err error ->
-                    toString error
-                        |> Debug.log
-                        |> always NoOp
-
-                Ok { data } ->
-                    AddSelectedUsers data
-    in
-        String.join "/" [ "admin", "api", "group", toString id, "users" ]
-            |> String.cons '/'
-            |> HttpBuilder.get
-            |> withHeaders [ "Accept" => "application/json" ]
-            |> withCredentials
-            |> attempt toMsg (jsonReader Decoder.users) stringReader
+    request Get [ "admin", "api", "group", toString id, "users" ] []
+        |> withBase
+        |> attempt (noOp AddSelectedUsers) (jsonReader Decoder.users) stringReader
 
 
 searchUsers : String -> Cmd Msg
 searchUsers query =
-    let
-        toMsg result =
-            case result of
-                Err error ->
-                    toString error
-                        |> Debug.log
-                        |> always NoOp
+    request Get [ "admin", "api", "user", "search" ] [ "q" => query ]
+        |> withBase
+        |> attempt (noOp SetSearchedUsers) (jsonReader Decoder.users) stringReader
 
-                Ok { data } ->
-                    SetSearchedUsers data
+
+url : List String -> List ( String, String ) -> String
+url paths params =
+    let
+        base =
+            String.join "/" paths
+                |> String.cons '/'
     in
-        String.join "/" [ "admin", "api", "user", "search" ++ "?q=" ++ query ]
-            |> String.cons '/'
-            |> HttpBuilder.get
-            |> withHeaders [ "Accept" => "application/json" ]
-            |> withCredentials
-            |> attempt toMsg (jsonReader Decoder.users) stringReader
+        HttpBuilder.url base params
+
+
+withBase : RequestBuilder -> RequestBuilder
+withBase builder =
+    builder
+        |> withHeaders [ "Accept" => "application/json" ]
+        |> withCredentials
+
+
+noOp : (a -> Msg) -> Result (Error b) (Response a) -> Msg
+noOp toMsg result =
+    case result of
+        Err error ->
+            toString error
+                |> Debug.log
+                |> always NoOp
+
+        Ok { data } ->
+            toMsg data
