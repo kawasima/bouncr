e33d421691c83bc3cf75773b0ade31c1145fdea3
kawasima <kawasima1016@gmail.com>
2017-11-24 12:31:15 +0900

    Fix #20

--- src/main/java/db/migration/V22__AlterInvitations.java
+++ src/main/java/db/migration/V22__AlterInvitations.java
@@ -2,11 +2,14 @@ package db.migration;
 
 import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 import org.jooq.impl.SQLDataType;
 
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.jooq.impl.DSL.*;
 
@@ -18,6 +21,16 @@ public class V22__AlterInvitations implements JdbcMigration {
             String ddl = create.alterTable(table("oidc_invitations"))
                     .renameColumn(field("oidc_sub")).to(field("oidc_payload", SQLDataType.CLOB))
                     .getSQL();
+            if (create.configuration().dialect() == SQLDialect.MYSQL) {
+                Matcher m = Pattern.compile("\\s+RENAME\\s+COLUMN\\s+(\\w+)\\s+TO\\s+", Pattern.CASE_INSENSITIVE).matcher(ddl);
+                StringBuffer sb = new StringBuffer();
+                if (m.find()) {
+                    m.appendReplacement(sb, " change " + m.group(1) + " ");
+                    m.appendTail(sb);
+                    ddl = sb.toString();
+                }
+            }
+
             stmt.execute(ddl);
         }
     }
