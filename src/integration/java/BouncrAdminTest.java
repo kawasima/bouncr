68c9c31ef537be8142f8c3664526a03e689c3bad
kawasima <kawasima1016@gmail.com>
2018-02-27 19:08:34 +0900

    Add configuration of circleci

--- src/integration/java/BouncrAdminTest.java
+++ src/integration/java/BouncrAdminTest.java
@@ -1,12 +1,18 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import enkan.Env;
 import enkan.config.EnkanSystemFactory;
 import enkan.system.EnkanSystem;
 import enkan.util.ReflectionUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Proxy;
+
+import java.util.Optional;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -23,6 +29,29 @@ public class BouncrAdminTest {
             EnkanSystemFactory enkanSystemFactory = (EnkanSystemFactory) enkanSystemFactoryClass.newInstance();
             return enkanSystemFactory.create();
         });
+
+        Optional.ofNullable(Env.get("http_proxy"))
+                .ifPresent(proxy -> WebDriverManager.chromedriver().proxy(proxy));
+
+        Configuration.browser = WebDriverRunner.CHROME;
+        Configuration.headless = true;
+        Configuration.reportsFolder = "target/integration-test/reports";
+        Configuration.browserSize = "1024x768";
+
+        final Proxy proxy = new Proxy();
+        Optional.ofNullable(Env.get("http_proxy"))
+                .ifPresent(p -> proxy.setHttpProxy(p));
+        Optional.ofNullable(Env.get("https_proxy"))
+                .ifPresent(p -> proxy.setSslProxy(p));
+        String noProxy = Env.get("no_proxy");
+        if (noProxy != null && !noProxy.isEmpty()) {
+            proxy.setNoProxy(noProxy);
+        } else {
+            proxy.setNoProxy("localhost");
+        }
+        if (proxy.getHttpProxy() != null || proxy.getSslProxy() != null) {
+            WebDriverRunner.setProxy(proxy);
+        }
     }
 
     @BeforeEach
@@ -41,6 +70,5 @@ public class BouncrAdminTest {
         $(By.name("account")).setValue("admin");
         $(By.name("password")).setValue("admin");
         $("button[type=submit]").click();
-        $("#submit").click();
     }
 }
