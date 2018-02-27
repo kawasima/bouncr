84dd327bdf62cbd3e39fe827e3ee3192b66040bd
kawasima <kawasima1016@gmail.com>
2018-02-27 23:05:31 +0900

    Fix the bug where http_proxy isn't set

--- src/integration/java/BouncrAdminTest.java
+++ src/integration/java/BouncrAdminTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import enkan.Env;
@@ -11,27 +12,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Proxy;
+import page.HomePage;
 
 import java.util.Optional;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static enkan.util.ReflectionUtils.tryReflection;
 
 public class BouncrAdminTest {
     static EnkanSystem system;
+    static int port;
 
     @BeforeAll
     public static void createSystem() {
-        System.out.println(Env.getInt("PORT", 3000));
+        tryReflection(() -> {
+            Class.forName("net.unit8.bouncr.authz.UserPermissionPrincipal");
+            Class.forName("net.unit8.bouncr.authz.UserPrincipal");
+            Class.forName("net.unit8.bouncr.sign.JwtHeader");
+            Class.forName("net.unit8.bouncr.sign.JwtClaim");
+            return true;
+        });
 
+        port = Env.getInt("PORT", 3000);
         system = ReflectionUtils.tryReflection(() -> {
             Class<?> enkanSystemFactoryClass = Class.forName("net.unit8.bouncr.BouncrEnkanSystem");
             EnkanSystemFactory enkanSystemFactory = (EnkanSystemFactory) enkanSystemFactoryClass.newInstance();
             return enkanSystemFactory.create();
         });
 
-        Optional.ofNullable(Env.get("http_proxy"))
-                .ifPresent(proxy -> WebDriverManager.chromedriver().proxy(proxy));
+        Optional.of(Env.get("http_proxy"))
+                .filter(p -> !p.isEmpty())
+                .ifPresent(p -> WebDriverManager.chromedriver().proxy(p));
 
         Configuration.browser = WebDriverRunner.CHROME;
         Configuration.headless = true;
@@ -39,15 +50,17 @@ public class BouncrAdminTest {
         Configuration.browserSize = "1024x768";
 
         final Proxy proxy = new Proxy();
-        Optional.ofNullable(Env.get("http_proxy"))
+        Optional.of(Env.get("http_proxy"))
+                .filter(p -> !p.isEmpty())
                 .ifPresent(p -> proxy.setHttpProxy(p));
         Optional.ofNullable(Env.get("https_proxy"))
+                .filter(p -> !p.isEmpty())
                 .ifPresent(p -> proxy.setSslProxy(p));
         String noProxy = Env.get("no_proxy");
-        if (noProxy != null && !noProxy.isEmpty()) {
-            proxy.setNoProxy(noProxy);
-        } else {
+        if (noProxy.isEmpty()) {
             proxy.setNoProxy("localhost");
+        } else {
+            proxy.setNoProxy(noProxy);
         }
         if (proxy.getHttpProxy() != null || proxy.getSslProxy() != null) {
             WebDriverRunner.setProxy(proxy);
@@ -66,9 +79,11 @@ public class BouncrAdminTest {
 
     @Test
     public void userCanLoginByUsername() {
-        open("http://localhost:3005/my/signIn");
+        open("http://localhost:" + port + "/my/signIn");
         $(By.name("account")).setValue("admin");
-        $(By.name("password")).setValue("admin");
+        $(By.name("password")).setValue("password");
         $("button[type=submit]").click();
+        HomePage homePage = page(HomePage.class);
+        homePage.header.shouldBe(Condition.text("Admin User"));
     }
 }
